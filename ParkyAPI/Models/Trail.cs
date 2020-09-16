d0a5efe6adbf3e379d5b877dff3c1c18cca99228
abayomi igwubor <igwubor@outlook.com>
2020-09-16 15:31:20 +0100

    addedTrailRepositoryPattern

--- ParkyAPI/Models/Trail.cs
+++ ParkyAPI/Models/Trail.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkyAPI.Models
 {
-    public class Trail
-    {
+	public class Trail
+	{
 		[Key]
 		public int Id { get; set; }
 		[Required]
 		public string  Name { get; set; }
+
+		[Required]
+
+		public double Distance { get; set; }
+
+		public enum DifficultyType { Easy,Moderate, Difficult, Expert}
+		public DifficultyType Difficulty { get;set; }
+		[Display(Name ="National Park")]
+		public int NationalParkId { get; set; }
+		[ForeignKey("NationalParkId")]
+		public NationalPark NationalPark { get; set; }
+
+		public DateTime DateCreated { get; set; }
 	}
 }
