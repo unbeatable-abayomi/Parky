39485270387c1f5bc2502002e56027a803ca8d48
abayomi igwubor <igwubor@outlook.com>
2020-10-01 09:23:31 +0100

    completedAPIVersionOne

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -38,12 +38,18 @@ namespace ParkyAPI
 			services.AddScoped<INationalParkRepository, NationalParkRepository>();
 			services.AddScoped<ITrailRepository, TrailRepository>();
 			services.AddAutoMapper(typeof(ParkyMappings));
+			services.AddApiVersioning(options =>
+			{
+				options.AssumeDefaultVersionWhenUnspecified = true;
+				options.DefaultApiVersion = new ApiVersion(1, 0);
+				options.ReportApiVersions = true;
+			});
 			services.AddSwaggerGen(options=> {
-				options.SwaggerDoc("ParkyOpenAPISpecNP",
+				options.SwaggerDoc("ParkyOpenAPISpec",
 					new Microsoft.OpenApi.Models.OpenApiInfo()
 					{
 
-						Title = "Parky API(National Park)",
+						Title = "Parky API",
 						Version = "1",
 						Description = "Abayomi's Parky API NP",
 						Contact = new Microsoft.OpenApi.Models.OpenApiContact()
@@ -60,27 +66,27 @@ namespace ParkyAPI
 						}
 
 					});
-				options.SwaggerDoc("ParkyOpenAPISpecTrails",
-	new Microsoft.OpenApi.Models.OpenApiInfo()
-	{
+	//			options.SwaggerDoc("ParkyOpenAPISpecTrails",
+	//new Microsoft.OpenApi.Models.OpenApiInfo()
+	//{
 
-		Title = "Parky API Trails",
-		Version = "1",
-		Description = "Abayomi's Parky API Trails",
-		Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-		{
-			Email = "igwubor@gmail.com",
-			Name = "Abayomi Igwubor",
-			Url = new Uri("https://abayomiigwubor.netlify.com/")
-		},
-		License = new Microsoft.OpenApi.Models.OpenApiLicense()
-		{
-			Name = "MIT License",
-			Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+	//	Title = "Parky API Trails",
+	//	Version = "1",
+	//	Description = "Abayomi's Parky API Trails",
+	//	Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+	//	{
+	//		Email = "igwubor@gmail.com",
+	//		Name = "Abayomi Igwubor",
+	//		Url = new Uri("https://abayomiigwubor.netlify.com/")
+	//	},
+	//	License = new Microsoft.OpenApi.Models.OpenApiLicense()
+	//	{
+	//		Name = "MIT License",
+	//		Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
 
-		}
+	//	}
 
-	});
+	//});
 				var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name }.xml";
 				var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
 				options.IncludeXmlComments(cmlCommentsFullPath);
@@ -100,8 +106,9 @@ namespace ParkyAPI
 			app.UseHttpsRedirection();
 			app.UseSwagger();
 			app.UseSwaggerUI(options => {
-				options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
-				options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
+				options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API ");
+				//options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
+				//options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
 				options.RoutePrefix = "";
 			
 			});
