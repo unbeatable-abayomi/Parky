b8a872119fabdf4c53c476d3ee51e2c04670b758
abayomi igwubor <igwubor@outlook.com>
2020-10-08 14:24:42 +0100

    addedParkyWeb

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44310/";
+        public static string NationalParkAPIPath = APIBaseUrl+ "api/v1/nationalParks";
+        public static string TrailAPIPath = APIBaseUrl+ "api/v1/trails";
+    }
+}
